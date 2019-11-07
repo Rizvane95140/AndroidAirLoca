35c3fcba20694a93ea607c456fa33c1946d4ec2c
Rizvane <rizvane95140@gmail.com>
2019-11-07 16:17:54 +0100

    Ajout splashscreen

--- app/src/main/java/com/example/airloca/MainActivity.java
+++ app/src/main/java/com/example/airloca/MainActivity.java
@@ -3,16 +3,42 @@ package com.example.airloca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
+        AsyncSleep asyncSleep = new AsyncSleep();
+        asyncSleep.execute();
     }
+
+    public class AsyncSleep extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s ) {
+            //Handle result here
+            super.onPostExecute(s);
+
+            Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
+
 }
